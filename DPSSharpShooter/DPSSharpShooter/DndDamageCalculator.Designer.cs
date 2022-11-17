d85f6bfe2ca41f97a851a2500286c45429f7ed92
Hunter6233 <118466593+Hunter6233@users.noreply.github.com>
2022-11-17 11:31:40 -0500

    Added existing code to the GitHub Repo

---
+++ DPSSharpShooter/DPSSharpShooter/DndDamageCalculator.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace DPSSharpShooter
+{
+    partial class DndDamageCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// 
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.attackText = new System.Windows.Forms.TextBox();
+            this.attackLabel = new System.Windows.Forms.Label();
+            this.acLabel = new System.Windows.Forms.Label();
+            this.acText = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.results = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.attackModifierText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.attackDieText = new System.Windows.Forms.TextBox();
+            this.sharpshooter = new System.Windows.Forms.CheckBox();
+            this.avgDamageText = new System.Windows.Forms.Label();
+            this.chanceToHitText = new System.Windows.Forms.Label();
+            this.maxHitNormalText = new System.Windows.Forms.Label();
+            this.maxHitCritText = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.advantage = new System.Windows.Forms.CheckBox();
+            this.disadvantage = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // attackText
+            // 
+            this.attackText.Location = new System.Drawing.Point(48, 48);
+            this.attackText.Name = "attackText";
+            this.attackText.Size = new System.Drawing.Size(100, 20);
+            this.attackText.TabIndex = 0;
+            this.attackText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // attackLabel
+            // 
+            this.attackLabel.AutoSize = true;
+            this.attackLabel.Location = new System.Drawing.Point(64, 32);
+            this.attackLabel.Name = "attackLabel";
+            this.attackLabel.Size = new System.Drawing.Size(71, 13);
+            this.attackLabel.TabIndex = 1;
+            this.attackLabel.Text = "Attack Bonus";
+            // 
+            // acLabel
+            // 
+            this.acLabel.AutoSize = true;
+            this.acLabel.Location = new System.Drawing.Point(64, 93);
+            this.acLabel.Name = "acLabel";
+            this.acLabel.Size = new System.Drawing.Size(62, 13);
+            this.acLabel.TabIndex = 3;
+            this.acLabel.Text = "Armor Class";
+            // 
+            // acText
+            // 
+            this.acText.Location = new System.Drawing.Point(48, 109);
+            this.acText.Name = "acText";
+            this.acText.Size = new System.Drawing.Size(100, 20);
+            this.acText.TabIndex = 2;
+            this.acText.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.calculateButton.Location = new System.Drawing.Point(48, 264);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(115, 40);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // results
+            // 
+            this.results.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.results.AutoSize = true;
+            this.results.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.results.Location = new System.Drawing.Point(435, 48);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(121, 31);
+            this.results.TabIndex = 5;
+            this.results.Text = "Results: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Attack Damage";
+            // 
+            // attackModifierText
+            // 
+            this.attackModifierText.Location = new System.Drawing.Point(228, 48);
+            this.attackModifierText.Name = "attackModifierText";
+            this.attackModifierText.Size = new System.Drawing.Size(100, 20);
+            this.attackModifierText.TabIndex = 6;
+            this.attackModifierText.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Attack Die (Sides)";
+            // 
+            // attackDieText
+            // 
+            this.attackDieText.Location = new System.Drawing.Point(228, 109);
+            this.attackDieText.Name = "attackDieText";
+            this.attackDieText.Size = new System.Drawing.Size(100, 20);
+            this.attackDieText.TabIndex = 8;
+            this.attackDieText.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // sharpshooter
+            // 
+            this.sharpshooter.AutoSize = true;
+            this.sharpshooter.Location = new System.Drawing.Point(48, 156);
+            this.sharpshooter.Name = "sharpshooter";
+            this.sharpshooter.Size = new System.Drawing.Size(203, 17);
+            this.sharpshooter.TabIndex = 10;
+            this.sharpshooter.Text = "Sharpshooter/Great Weapon Bonus?";
+            this.sharpshooter.UseVisualStyleBackColor = true;
+            // 
+            // avgDamageText
+            // 
+            this.avgDamageText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.avgDamageText.AutoSize = true;
+            this.avgDamageText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.avgDamageText.Location = new System.Drawing.Point(562, 54);
+            this.avgDamageText.Name = "avgDamageText";
+            this.avgDamageText.Size = new System.Drawing.Size(289, 25);
+            this.avgDamageText.TabIndex = 11;
+            this.avgDamageText.Text = "Average Damage Per Attack:";
+            // 
+            // chanceToHitText
+            // 
+            this.chanceToHitText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chanceToHitText.AutoSize = true;
+            this.chanceToHitText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chanceToHitText.Location = new System.Drawing.Point(562, 204);
+            this.chanceToHitText.Name = "chanceToHitText";
+            this.chanceToHitText.Size = new System.Drawing.Size(155, 25);
+            this.chanceToHitText.TabIndex = 12;
+            this.chanceToHitText.Text = "Chance To Hit:";
+            this.chanceToHitText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // maxHitNormalText
+            // 
+            this.maxHitNormalText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maxHitNormalText.AutoSize = true;
+            this.maxHitNormalText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxHitNormalText.Location = new System.Drawing.Point(562, 156);
+            this.maxHitNormalText.Name = "maxHitNormalText";
+            this.maxHitNormalText.Size = new System.Drawing.Size(165, 25);
+            this.maxHitNormalText.TabIndex = 13;
+            this.maxHitNormalText.Text = "Max Hit Normal:";
+            // 
+            // maxHitCritText
+            // 
+            this.maxHitCritText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.maxHitCritText.AutoSize = true;
+            this.maxHitCritText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxHitCritText.Location = new System.Drawing.Point(562, 103);
+            this.maxHitCritText.Name = "maxHitCritText";
+            this.maxHitCritText.Size = new System.Drawing.Size(179, 25);
+            this.maxHitCritText.TabIndex = 14;
+            this.maxHitCritText.Text = "Max Hit With Crit:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(776, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Program by Matteo Genoese-Zerbi";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // advantage
+            // 
+            this.advantage.AutoSize = true;
+            this.advantage.Location = new System.Drawing.Point(48, 204);
+            this.advantage.Name = "advantage";
+            this.advantage.Size = new System.Drawing.Size(84, 17);
+            this.advantage.TabIndex = 16;
+            this.advantage.Text = "Advantage?";
+            this.advantage.UseVisualStyleBackColor = true;
+            this.advantage.CheckedChanged += new System.EventHandler(this.advantage_CheckedChanged);
+            // 
+            // disadvantage
+            // 
+            this.disadvantage.AutoSize = true;
+            this.disadvantage.Location = new System.Drawing.Point(210, 204);
+            this.disadvantage.Name = "disadvantage";
+            this.disadvantage.Size = new System.Drawing.Size(98, 17);
+            this.disadvantage.TabIndex = 17;
+            this.disadvantage.Text = "Disadvantage?";
+            this.disadvantage.UseVisualStyleBackColor = true;
+            this.disadvantage.CheckedChanged += new System.EventHandler(this.disadvantage_CheckedChanged);
+            // 
+            // DndDamageCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 315);
+            this.Controls.Add(this.disadvantage);
+            this.Controls.Add(this.advantage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maxHitCritText);
+            this.Controls.Add(this.maxHitNormalText);
+            this.Controls.Add(this.chanceToHitText);
+            this.Controls.Add(this.avgDamageText);
+            this.Controls.Add(this.sharpshooter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.attackDieText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.attackModifierText);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.acLabel);
+            this.Controls.Add(this.acText);
+            this.Controls.Add(this.attackLabel);
+            this.Controls.Add(this.attackText);
+            this.Name = "DndDamageCalculator";
+            this.Text = "D&D Damage Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox attackText;
+        private System.Windows.Forms.Label attackLabel;
+        private System.Windows.Forms.Label acLabel;
+        private System.Windows.Forms.TextBox acText;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label results;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox attackModifierText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox attackDieText;
+        private System.Windows.Forms.CheckBox sharpshooter;
+        private System.Windows.Forms.Label avgDamageText;
+        private System.Windows.Forms.Label chanceToHitText;
+        private System.Windows.Forms.Label maxHitNormalText;
+        private System.Windows.Forms.Label maxHitCritText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox advantage;
+        private System.Windows.Forms.CheckBox disadvantage;
+    }
+}
+
---
+++ DPSSharpShooter/DPSSharpShooter/DndDamageCalculator.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml.Schema;
+
+public enum AdvantageType
+{
+    normal,
+    advantage,
+    disadvantage
+}
+
+namespace DPSSharpShooter
+{
+    public partial class DndDamageCalculator : Form
+    {
+        private AdvantageType advantageType = AdvantageType.normal;
+        private int ac = 15; 
+        private int attack = 7; 
+        private int attackDamage = 10;
+        private int dieSides = 6;                                                           
+        private float avgDPS; 
+        private float attackDieAverage = 0;
+        private int maxHit;
+        private float chanceToHit = 0;
+        
+        public DndDamageCalculator() 
+        { 
+            InitializeComponent();
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            attackText.Text = attack.ToString();
+            acText.Text = ac.ToString();
+            attackModifierText.Text = attackDamage.ToString();
+            attackDieText.Text = dieSides.ToString();
+            DieSideCalculation();
+        }
+        private void DieSideCalculation() 
+        {
+            try
+            {
+                dieSides = Int32.Parse(attackDieText.Text);
+            }
+            catch
+            {
+                Console.WriteLine("Parse Failed");
+                dieSides = 0;
+            }
+            
+            attackDieAverage = 0;
+            for (int i = dieSides; i > 0; i--) 
+            { 
+                attackDieAverage += i;
+            } 
+            attackDieAverage = attackDieAverage/(float)dieSides;
+            //Console.WriteLine(dieSides.ToString());
+            //Console.WriteLine(attackDieAverage.ToString());
+        }
+
+        private bool ChanceToHitCalculator(int roll)
+        {
+            if (sharpshooter.Checked)
+            {
+                return roll + attack - 5 >= ac;
+            }
+            else
+            {
+                return roll + attack >= ac;
+            }
+        }
+        private int MaxHitCalculator(bool crit)
+        {
+            if (crit)
+            {
+                maxHit = attackDamage + (dieSides * 2);
+            }
+            else
+            {
+                maxHit = attackDamage + dieSides;
+            }
+            
+            if (sharpshooter.Checked)
+            {
+                maxHit += 10;
+            }
+            return maxHit;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) 
+        {
+            checkText(ref attackText, ref attack);
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e) 
+        {
+            checkText(ref acText, ref ac);
+        }
+        private void textBox1_TextChanged_1(object sender, EventArgs e) 
+        {
+            checkText(ref attackModifierText, ref attackDamage);
+        }
+        private void textBox2_TextChanged_1(object sender, EventArgs e) 
+        {
+            checkText(ref attackDieText, ref dieSides);
+            DieSideCalculation();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e) 
+        {
+            CheckAll();
+            CalculateResults();
+        }
+        private void CheckAll()
+        {
+            checkText(ref attackDieText, ref dieSides);
+            checkText(ref attackText, ref attack);
+            checkText(ref acText, ref ac);
+            checkText(ref attackModifierText, ref attackDamage);
+        }
+        private void CalculateResults() 
+        {
+            DieSideCalculation();
+            DisplayResults();
+        }
+
+        private void DisplayResults()
+        {
+            maxHitNormalText.Text = "Max Hit Normal: " + MaxHitCalculator(false);
+            maxHitCritText.Text = "Max hit with Crit: " + MaxHitCalculator(true);
+            avgDamageText.Text = "Average Damage Per Attack: " + CalculateAverageDPS();
+            chanceToHitText.Text = "Chance to Hit: " + chanceToHit.ToString() + "%";
+        }
+        private float CalculateAverageDPS()
+        {
+            chanceToHit = 0;
+            avgDPS = 0;
+            //Console.WriteLine("AC: " + ac);
+            switch (advantageType)
+            {
+                case AdvantageType.normal:
+                    return NormalAvgCalc();
+                case AdvantageType.advantage:
+                    return AdvantageAvgCalc(true);
+                case AdvantageType.disadvantage:
+                    return AdvantageAvgCalc(false);
+                default:
+                    Console.WriteLine("Error, no advantage type selected");
+                    return 0;
+            }
+        }
+
+        private float NormalAvgCalc()
+        {
+            for (int i = 2; i < 20; i++)
+            {
+                if (ChanceToHitCalculator(i))
+                {
+                    chanceToHit++;
+                    Console.WriteLine(chanceToHit.ToString());
+                    if (!sharpshooter.Checked)
+                    {
+                        avgDPS += attackDamage + attackDieAverage;
+                    }
+                    else
+                    {
+                        avgDPS += attackDamage + attackDieAverage + 10;
+                    }
+                }
+            }
+            avgDPS += attackDamage + (attackDieAverage * 2);
+            avgDPS = avgDPS / 20;
+
+            chanceToHit = ((chanceToHit )* 5);
+
+            return avgDPS;
+        }
+        private float AdvantageAvgCalc(bool advantage)
+        {
+            if (advantage)
+            {
+                for(int roll1 = 1; roll1 <= 20; roll1++)
+                {
+                    for (int roll2 = 1; roll2 <= 20; roll2++)
+                    {
+                        
+                        if (roll1 == 1 && roll2 == 1)
+                        {
+                            
+                        }
+                        else if(roll1 == 20 || roll2 == 20)
+                        {
+                            avgDPS += CritDamage(sharpshooter.Checked);
+                            chanceToHit++;
+                        }
+                        else if (ChanceToHitCalculator(roll1) || ChanceToHitCalculator(roll2))
+                        {
+                            avgDPS += NormalDamage(sharpshooter.Checked);
+                            chanceToHit++;
+
+                        }
+                        
+                    }
+                }
+            }
+            else
+            {
+                for (int roll1 = 1; roll1 <= 20; roll1++)
+                {
+                    for (int roll2 = 1; roll2 <= 20; roll2++)
+                    {
+                        if (!(roll1 == 1 )|| !(roll2 == 1))
+                        {
+                            if (roll1 == 20 && roll2 == 20)
+                            {
+                                avgDPS += CritDamage(sharpshooter.Checked);
+                                chanceToHit++;
+                            }
+                            else if (ChanceToHitCalculator(roll1) && ChanceToHitCalculator(roll2))
+                            {
+                                avgDPS += NormalDamage(sharpshooter.Checked);
+                                chanceToHit++;
+                            }
+                        }
+
+                    }
+                }
+            }
+            chanceToHit = chanceToHit * 0.25f;
+
+            avgDPS /= 400;
+
+            return avgDPS;
+        }
+
+        public float CritDamage(bool sharpshooting)
+        {
+            float result = attackDamage + (attackDieAverage * 2);
+            if (sharpshooting)
+            {
+                result += 10;
+            }
+            
+            return result;
+        }
+        public float NormalDamage(bool sharpshooting)
+        {
+            float result = attackDamage + attackDieAverage;
+            if (sharpshooting)
+            {
+                result += 10;
+            }
+
+            return result;
+        }
+
+        private void checkText(ref TextBox textBox, ref int number)
+        {
+
+            try
+            {
+                Console.WriteLine("Before, the value of " + textBox.Name + " is set to " + number);
+                number = Int32.Parse(textBox.Text);
+                Console.WriteLine("After, the value of " + textBox.Name + " is set to " + number);
+            }
+            catch
+            {
+                Console.WriteLine("Parse Failed!");
+                if (textBox.Text != "")
+                {
+                    textBox.Text = number.ToString();
+                    //Console.WriteLine(textBox.ToString() + " is set to " + number);
+                }
+                else
+                {
+                    number = 0;
+                }
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.matteogenoese-zerbi.com/");
+        }
+
+        private void disadvantage_CheckedChanged(object sender, EventArgs e)
+        {
+            if (disadvantage.Checked)
+            {
+                advantage.Checked = false;
+                advantage.CheckState = CheckState.Unchecked;
+                advantageType = AdvantageType.disadvantage;
+            }
+            else
+            {
+                advantageType = AdvantageType.normal;
+            }
+        }
+        private void advantage_CheckedChanged(object sender, EventArgs e)
+        {
+            if (advantage.Checked)
+            {
+                disadvantage.Checked = false;
+                disadvantage.CheckState = CheckState.Unchecked;
+                advantageType = AdvantageType.advantage;
+            }
+            else
+            {
+                advantageType = AdvantageType.normal;
+            }
+        }
+    }
+}
